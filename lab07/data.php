e558188085b377e283df019a742515993c375bad
Miguel Emmara <m.miguelemmara@gmail.com>
2022-05-11 12:16:56 +1200

    fix lab07, it was the space on "&pwd ="

--- lab07/data.php
+++ lab07/data.php
@@ -19,13 +19,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$query = "SELECT * FROM $table WHERE Names = '$name'";
+$query = "SELECT * FROM $table WHERE Names = '$name' AND Password = '$pwd'";
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
   // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "Email: ". $row["Email_Address"];
+  while ($row = $result->fetch_assoc()) {
+    echo "Email: " . $row["Email_Address"];
     echo "<br>";
   }
 } else {
@@ -33,40 +33,8 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 
-
-
-// @mysqli_select_db($conn, $stuID) or die("Cant select the database");
-// if (!$conn) {
-//     echo "<p>Database connection failure</p>";
-// } else {
-
-//     $query = "select * from '$table' where name = '$name';";
-
-//     $result = mysqli_query($conn, $query);
-
-//     if (!$result) {
-//         echo "<p>Something is wrong with ",    $query, "</p>";
-//     } else {
-
-
-
-//         while ($row = mysqli_fetch_assoc($result)) {
-
-//             echo "email: ", $row["name"];
-//         }
-
-
-//         mysqli_free_result($result);
-//     } // if successful query operation
-
-//     // close the database connection
-//     mysqli_close($conn);
-// } // if successful database connection	
-
-
-
 // sleep for 10 seconds to slow server response down
 sleep(1);
 // write back the password concatenated to end of the name
-echo ($name . " : " . $pwd);
+// echo ($name . " : " . $pwd);
 ?>
--- lab07/simpleajax.js
+++ lab07/simpleajax.js
@@ -1,44 +1,11 @@
 // file simpleajax.js
-// var xhr = createRequest();
-
-// function getData(dataSource, divID, aName, aPwd) {
-// 	if (xhr) {
-// 		var place = document.getElementById(divID);
-// 		var url = dataSource + "?name=" + aName + "&pwd=" + aPwd;
-// 		xhr.open("POST", url, true);
-// 		xhr.onreadystatechange = function() {
-// 			alert(xhr.readyState);
-// 			if (xhr.readyState == 4 && xhr.status == 200) {
-// 				place.innerHTML = xhr.responseText;
-// 			} // end if
-// 		} // end anonymous call-back function
-// 		xhr.send(null);
-// 	} // end if
-// } // end function getData()
-
-// var xhr = createRequest();
-
-// function getData(dataSource, divID, aName, aPwd) {
-//     if (xhr) {
-//         var place = document.getElementById(divID);
-//         var url = dataSource + "?name=" + aName + "&pwd=" + aPwd;
-//         xhr.open("POST", url, true);
-//         xhr.onreadystatechange = function() {
-//                 alert(xhr.readyState);
-//                 if (xhr.readyState == 4 && xhr.status == 200) {
-//                     place.innerHTML = xhr.responseText;
-//                 } // end if
-//             } // end anonymous call-back function
-//         xhr.send(null);
-//     } // end if
-// } // end function getData()
 
 var xhr = createRequest();
 
 function getData(dataSource, divID, aName, aPwd) {
     if (xhr) {
         var obj = document.getElementById(divID);
-        var requestbody = "name=" + encodeURIComponent(aName) + "  &pwd = " + encodeURIComponent(aPwd);
+        var requestbody = "name=" + encodeURIComponent(aName) + "&pwd=" + encodeURIComponent(aPwd);
         xhr.open("POST", dataSource, true);
         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhr.onreadystatechange = function() {
