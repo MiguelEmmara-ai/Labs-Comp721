08271279df2d37bede808bf7d63a0519e1f5eb55
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-06-05 15:19:58 +1200

    Some work on lab 10 task 2

---
+++ lab10/AngularLab/course-info-app/src/app/test-course.ts
@@ -0,0 +1,11 @@
+import { Course } from './course';
+
+export const COURSES: Course[] = [
+     // ----insert array of five courses with ID here-----
+     // TODO
+     { course_id: 1, course_title: "Web Development", semester: "One", period: "Wednesday 4-6 PM", lecturer: "Dr. Jian Yu"},
+     { course_id: 1, course_title: "Web Development", semester: "One", period: "Wednesday 4-6 PM", lecturer: "Dr. Jian Yu"},
+     { course_id: 1, course_title: "Web Development", semester: "One", period: "Wednesday 4-6 PM", lecturer: "Dr. Jian Yu"},
+     { course_id: 1, course_title: "Web Development", semester: "One", period: "Wednesday 4-6 PM", lecturer: "Dr. Jian Yu"},
+     { course_id: 1, course_title: "Web Development", semester: "One", period: "Wednesday 4-6 PM", lecturer: "Dr. Jian Yu"}
+];
